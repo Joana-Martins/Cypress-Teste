1bb4732599d4496c481144f9cefd95568b6460c7
Joana <joana.santoss.martins@gmail.com>
2023-06-26 18:23:09 -0300

    before each

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login', () => {
+  beforeEach(() => {
     cy.visit('https://inf15979.disciplinas.rlaiola.inf.ufes.br/boca/')
     cy.get(':nth-child(1) > :nth-child(2) > input').type('ihc')
     cy.get(':nth-child(2) > :nth-child(2) > input').type('ihc')
     cy.get('[type="submit"]').click()
-    cy.get(':nth-child(1) > .menu')
+  })
+
+  it('passes', () => {
+       cy.get(':nth-child(1) > .menu').click()
+  })
+
+  it('clicar', () => {
+      cy.get(':nth-child(3) > .menu').click()
   })
 })
+
+
+
+
+
+
